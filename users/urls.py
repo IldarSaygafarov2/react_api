7c72961a84e1282a47ce27a2ff2e1b2ffedd5673
Sergey <sergey.01.31.93@gmail.com>
2024-02-08 13:13:14 +0500

    added users crud operations

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import MyObtainTokenPairView, RegisterView, UserAvatarUpdateView
+
+urlpatterns = [
+    path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
+    path('users/<int:pk>/update/avatar/', UserAvatarUpdateView.as_view())
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from .models import User
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer, UpdateUserAvatarSerializer
+
+
+class MyObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class UserAvatarUpdateView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UpdateUserAvatarSerializer
